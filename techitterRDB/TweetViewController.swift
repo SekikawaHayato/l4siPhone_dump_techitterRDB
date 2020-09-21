0eb022a2c6a9c9f563879b7fe3e4e9dd155f4bab
SekikawaHayato <hayato.901i.softbank@gmail.com>
2020-09-22 00:54:44 +0900

    ログイン画面作成

---
+++ techitterRDB/TweetViewController.swift
@@ -0,0 +1,32 @@
+//
+//  TweetViewController.swift
+//  techitterRDB
+//
+//  Created by 関川隼人 on 2020/09/22.
+//  Copyright © 2020 関川隼人. All rights reserved.
+//
+
+import UIKit
+
+class TweetViewController: UIViewController {
+
+    @IBOutlet var name: String!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- techitterRDB/ViewController.swift
+++ techitterRDB/ViewController.swift
@@ -8,13 +8,30 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController ,UITextFieldDelegate{
+    
+    @IBOutlet var nameTextField: UITextField!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        nameTextField.delegate = self
     }
 
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    @IBAction func toTwiite(){
+        performSegue(withIdentifier: "toTweet", sender: nameTextField.text)
+    }
 
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+           if segue.identifier == "toTweet" {
+               let twiiteViewController = segue.destination as! TweetViewController
+               twiiteViewController.name = sender as? String
+           }
+       }
 }
 
